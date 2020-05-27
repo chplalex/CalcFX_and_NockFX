cc2ae1c50b373cc8ea1cb994e3049bdc47c9da9b
Alexander Chepel <chepel.alexander@gmail.com>
2020-05-27 10:51:28 +0300

    NockFX messenger prototype.

---
+++ src/NockFX/NockFXApp.java
@@ -0,0 +1,89 @@
+package NockFX;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class NockFXApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        final double SPACING = 3;
+        final double WIDTH_PROPORTION = 0.25;
+        final double HEIGHT_PROPORTION = 0.85;
+
+        VBox root = new VBox(SPACING);
+        root.setAlignment(Pos.TOP_CENTER);
+
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        double stageWidth = d.width * WIDTH_PROPORTION;
+        double stageHeight = d.height * HEIGHT_PROPORTION;
+        stage.setWidth(stageWidth);
+        stage.setHeight(stageHeight);
+        stage.setTitle("NockFX messenger");
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        ObservableList nodeList = root.getChildren();
+
+        TextArea textOut = new TextArea();
+        textOut.setEditable(false);
+        textOut.setFocusTraversable(false);
+
+        TextArea textIn = new TextArea();
+        textIn.setEditable(true);
+        textIn.setFocusTraversable(true);
+        textIn.setOnKeyPressed((event) -> {
+            if (event.getCode().equals(KeyCode.ENTER)) {
+                doTextSend(textIn, textOut);
+            }
+        });
+
+        Button btnSend = new Button("Send");
+        btnSend.setPrefWidth(stageWidth);
+        btnSend.setFocusTraversable(false);
+        btnSend.setOnAction((actionEvent) -> doTextSend(textIn, textOut));
+
+        textIn.setPrefHeight(btnSend.getHeight() * 2);
+        textOut.setPrefHeight(stageHeight - btnSend.getHeight() - textOut.getHeight() - 2 * SPACING);
+        stage.setMinHeight(300);
+
+        nodeList.addAll(textOut, textIn, btnSend);
+
+//        ChangeListener<Number> listener = ((observable, oldValue, newValue) -> {
+//            textOut.clear();
+//            textOut.appendText("Stage: W = " + stage.getWidth() + ". H = " + stage.getHeight() + "\n");
+//            textOut.appendText("Root : W = " + root.getWidth() + ". H = " + root.getHeight() + "\n");
+//            textOut.appendText("Scene: W = " + scene.getWidth() + ". H = " + scene.getHeight() + "\n");
+//            textOut.appendText("txtOu: W = " + textOut.getWidth() + ". H = " + textOut.getHeight() + "\n");
+//            textOut.appendText("txtIn: W = " + textIn.getWidth() + ". H = " + textIn.getHeight() + "\n");
+//            textOut.appendText("buttn: W = " + btnSend.getWidth() + ". H = " + btnSend.getHeight() + "\n");
+//        });
+
+//        stage.widthProperty().addListener(listener);
+//        stage.heightProperty().addListener(listener);
+
+        stage.show();
+    }
+
+    private void doTextSend(TextArea textIn, TextArea textOut) {
+        textOut.appendText(textIn.getText());
+        textIn.clear();
+    }
+
+}
